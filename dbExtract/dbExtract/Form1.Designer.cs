a412a4f9430b085fd5f9b907e3b323b65a91f7bc
yfan7400 <yfan7400@uni.sydney.edu.au>
2016-08-22 14:23:23 +1000

    First upload commit.
    This version is basically usable, but not optimized and may have bugs.

---
+++ dbExtract/dbExtract/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace dbExtract
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择Collection txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "选择Osu文件夹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(34, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "完成！快去看看Osu！文件夹里的MoonLight文件夹吧！";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(-1, 237);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(357, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(-1, 208);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(357, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 261);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "小月的Osu收藏提取";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ dbExtract/dbExtract/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dbExtract
+{
+    public partial class Form1 : Form
+    {
+        public static ArrayList codelist = new ArrayList();
+        public static int progress = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static List<int> AllIndexesOf(string str, string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", "value");
+            List<int> indexes = new List<int>();
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    return indexes;
+                indexes.Add(index);
+            }
+        }
+
+
+        public static string findNumber(string line) {
+            //string line = ;
+            int frontIndex = line.IndexOf("- ");
+            string cropfront = line.Substring(frontIndex+2);
+            int cropback = cropfront.IndexOf("\n");
+            string code = cropfront.Substring(0, cropback);
+            return code;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                    List<int> indexes = AllIndexesOf(text, "http://osu.ppy.sh/b/");
+                    
+                    foreach (int i in indexes)
+                    {
+                        codelist.Add(findNumber(text.Substring(i)));
+                    }
+                    ArrayList a = codelist;
+                    Console.WriteLine(size);
+                    progressBar1.Maximum = codelist.Count;
+
+                }
+                catch (IOException)
+                {
+                }
+            }
+            System.Diagnostics.Debug.WriteLine(size); // <-- Shows file size in debugging mode.
+            System.Diagnostics.Debug.WriteLine(result); // <-- For debugging use.
+        }
+
+        public static void DeepCopy(DirectoryInfo source, DirectoryInfo target)
+        {
+
+            // Recursively call the DeepCopy Method for each Directory
+            foreach (DirectoryInfo dir in source.GetDirectories())
+                DeepCopy(dir, target.CreateSubdirectory(dir.Name));
+
+            // Go ahead and copy each file in "source" to the "target" directory
+            foreach (FileInfo file in source.GetFiles()) {
+                if (!System.IO.File.Exists(Path.Combine(target.FullName, file.Name)))
+                {
+                    file.CopyTo(Path.Combine(target.FullName, file.Name));
+                }
+            }
+                
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progress = 0;
+            int size = -1;
+            DialogResult result = folderBrowserDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = folderBrowserDialog1.SelectedPath;
+                string targetPath = file + "\\MoonLight";
+                
+                try
+                {
+                    
+                    string[] songs = Directory.GetDirectories(file+"\\Songs");
+                    progressBar1.Maximum = songs.Length;
+                    int max1 = songs.Length;
+                    foreach (string sourcePath in songs)
+                    {
+                        progress++;
+                        if (progress > progressBar1.Maximum) progress = progressBar1.Maximum;
+                        progressBar1.Value = progress;
+                        //label3.Text = progress + "/" + max1;
+                        int percent1 = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+                        progressBar1.CreateGraphics().DrawString(percent1.ToString() + "%", new Font("Arial", (float)8.25, FontStyle.Regular), Brushes.Black, new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+                        progressBar2.Maximum = codelist.Count;
+                        int max2 = codelist.Count;
+                        int counter = 0;
+                        foreach (string code in codelist) {
+                            counter++;
+                            progressBar2.Value = counter;
+                            //label2.Text = counter+"/"+max2;
+
+                            if ((counter == 246)&&(progress==11)) {
+                                Console.WriteLine();
+                            }
+                            if (sourcePath.Contains(code)){
+                                
+                                string innertargetpath = targetPath + "\\" + sourcePath.Substring(sourcePath.LastIndexOf("\\") + 1);
+                                if (!System.IO.Directory.Exists(innertargetpath)) ;
+                                {
+                                    System.IO.Directory.CreateDirectory(innertargetpath);
+                                }
+                                DirectoryInfo target = new DirectoryInfo(innertargetpath);
+                                //if (System.IO.Directory.Exists(sourcePath))
+                                //{
+                                //    string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                                //    // Copy the files and overwrite destination files if they already exist.
+                                //    foreach (string s in files)
+                                //    {
+                                //        // Use static Path methods to extract only the file name from the path.
+                                //        string fileName = System.IO.Path.GetFileName(s);
+                                //        string destFile = System.IO.Path.Combine(targetPath, fileName);
+                                //        System.IO.File.Copy(s, destFile, true);
+                                //    }
+                                //}
+                                DirectoryInfo source = new DirectoryInfo(sourcePath);
+                                DeepCopy(source, target);
+                            }
+                        }
+                    }
+                    label1.ForeColor = Color.Black;
+                    
+                    //ArrayList a = codelist;
+                    //Console.WriteLine(size);
+
+                }
+                catch (IOException)
+                {
+
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
